8b22eb83241caeb3de68e4d0076574999b3b72fe
Vent-Tardar <varvara270802@bk.ru>
2021-04-30 15:49:12 +0300

    Commit

---
+++ src/test/java/com/genusiic/vt/sprng/SprngApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genusiic.vt.sprng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprngApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
