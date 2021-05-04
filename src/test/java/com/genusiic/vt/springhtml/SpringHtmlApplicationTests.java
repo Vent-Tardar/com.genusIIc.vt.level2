cdb5feea77e1893ffc888049df1650f0b2da8dd0
Vent-Tardar <varvara270802@bk.ru>
2021-05-04 13:37:39 +0300

    DCF-75 Start

---
+++ src/test/java/com/genusiic/vt/springhtml/SpringHtmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genusiic.vt.springhtml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHtmlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
